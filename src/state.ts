5f331bda15b19cf4282803917d49bd362455fcf7
Zhi'en Foo <zfoo0004@student.monash.edu>
2023-08-29 18:41:30 +0800

    move some codes from main to state.ts and types.ts

---
+++ src/state.ts
@@ -0,0 +1,187 @@
+export { processEvent, TickEvent, InputEvent, clearGame, initialState }
+import type { State, Direction, Tetromino } from "./types"
+import { Constants, Block } from "./types";
+
+/** Event that represents an input */
+class InputEvent {
+  constructor(public readonly direction: Direction) {}
+}
+
+/** Event that represents a tick to update the rectangle position */
+class TickEvent {
+  constructor() {}
+}
+
+const randomBrick = () => tetrominoGreen;
+
+const clearGame = () =>
+  Array(Constants.GRID_HEIGHT)
+    .fill(0)
+    .map(e => Array(Constants.GRID_WIDTH).fill(0));
+
+const tetrominoGreen: Tetromino = { 
+  shape: [
+    [1], 
+  ], 
+  topLeft: {
+    row: 0,
+    col: 4
+  },
+  potentialX: 0,
+  potentialY: 4,
+  props: {
+    height: Block.HEIGHT,
+    width: Block.WIDTH,
+    x: Block.WIDTH,
+    y: Block.HEIGHT,
+    style: "fill: green",
+  }
+};
+
+const initialState: State = {
+  grid: clearGame(),
+  gameEnd: false,
+  col: 4, // starting topLeft x of green
+  row: 0, // starting topLeft y of green
+  currentTetromino: tetrominoGreen
+} as const;
+
+/** Processing state */
+
+/**
+ * Update state based on the event that comes in.
+ */
+const processEvent = (event: InputEvent | TickEvent, state: State): State => {
+  if (event instanceof TickEvent) return tick(state);
+  if (event instanceof InputEvent)
+    return move(state, event.direction);
+
+  return state;
+};
+
+/**
+ * Updates the state by proceeding with one time step.
+ *
+ * @param s Current state
+ * @returns Updated state
+ */
+const tick = (s: State): State => {
+  // Update the tetromino's position
+  const newState = ({
+    ...s,
+    row: s.row + 1,
+  });
+
+  return checkCollisions(newState);
+};
+
+/**
+ * Update the state to change the direction the rectangle
+ * is going in.
+ */
+const move = (state: State, direction: Direction): State => {
+  switch (direction) {
+    case "left":
+    case "right":
+      return checkCollisions(checkBounds(state), direction);
+    case "down":
+      return checkCollisions(checkBounds({ ...state, row: state.row + 1 }));
+  }
+};
+
+/**
+ * Ensure the rectangle is within bounds.
+ */
+const checkBounds = (state: State): State => {
+  const boundedX = boundX(state);
+  const boundedXY = boundY(boundedX);
+  return boundedXY;
+};
+
+/**
+ * Updates the state to ensure the rectangle stays within
+ * the x-axis bounds. Will reflect the rectangle in the x-axis if
+ * it was out of bounds.
+ */
+const boundX = (state: State): State => {
+  const { col: x, ...rest } = state;
+
+  if (x < 0) {
+    return { col: 0, ...rest };
+  }
+
+  if (x > Constants.GRID_WIDTH - 1) {
+    return { col: Constants.GRID_WIDTH - 1, ...rest };
+  }
+  return state;
+};
+
+/**
+ * Updates the state to ensure the rectangle stays within
+ * the y-axis bounds. Will reflect the rectangle in the y-axis if
+ * it was out of bounds.
+ */
+const boundY = (state: State): State => {
+  const { row: y, ...rest } = state;
+
+  if (y < 0) {
+    return { row: 0, ...rest };
+  }
+
+  if (y > Constants.GRID_HEIGHT - 1) {
+    return { row: Constants.GRID_HEIGHT - 1, ...rest }; 
+  }
+  return state;
+};
+
+const isCollidingAtCell = (state: State, i: number, j: number, deltaRow: number = 0, deltaCol: number = 0): boolean => {
+  const isOnGround = state.row + state.currentTetromino.shape.length > Constants.GRID_HEIGHT - 1
+  if (state.currentTetromino.shape[i][j] !== 0) {
+    return !isOnGround && state.grid[i + state.row + deltaRow][j + state.col + deltaCol] !== 0;
+  }
+  return false;
+};
+
+const isStackingOnBlocks = (state: State): boolean => {
+  return (state.currentTetromino.shape.some((row, i) => row.some((_, j) => isCollidingAtCell(state, i, j, 1, 0))))
+}
+
+const checkStackingOnBlocks = (state: State): State => {
+  if (isStackingOnBlocks(state)) {
+    return tetrominoLanded(state);
+  }
+  return state;
+};
+
+const checkSideCollisions = (state: State, direction: Direction): State => {
+  const deltaCol = direction === "left" ? -1 : 1
+
+  if (state.currentTetromino.shape.some((row, i) => row.some((_, j) => isCollidingAtCell(state, i, j, 0, deltaCol)))) {
+    return state;
+  }
+  return checkStackingOnBlocks({ ...state, col: state.col + deltaCol });
+};
+
+const checkCollisions = (state: State, direction: Direction | null = null): State => {
+  const isOnGround = state.row + state.currentTetromino.shape.length > Constants.GRID_HEIGHT - 1
+
+  return direction ? checkSideCollisions(state, direction) : isOnGround ? tetrominoLanded(state) : checkStackingOnBlocks(state);
+}
+
+/** Lands the current tetromino and adds it to grid */
+const tetrominoLanded = (s: State): State => {
+  const newGrid = clearGame();
+
+  s.grid.forEach((row, i) => 
+    row.forEach((col, j) => 
+      newGrid[i][j] = col));
+
+  s.currentTetromino.shape.forEach((row, i) => 
+    row.forEach((col, j) => {
+      newGrid[i + s.row][j + s.col] = col
+    }))
+
+  const newState = { ...s, grid: newGrid, col: 4, row: 0, currentTetromino: randomBrick() }
+  // end game if newly generated tetromino immediately collides with row below
+  return isStackingOnBlocks(newState) ? { ...newState, gameEnd: true } : newState;
+};
---
+++ src/types.ts
@@ -0,0 +1,60 @@
+export { Viewport, Constants, Block }
+export type { Key, Event, Direction, TetrominoProps, Tetromino, State }
+
+/** Constants */
+
+const Viewport = {
+  CANVAS_WIDTH: 200,
+  CANVAS_HEIGHT: 400,
+  PREVIEW_WIDTH: 160,
+  PREVIEW_HEIGHT: 80,
+} as const;
+
+const Constants = {
+  TICK_RATE_MS: 1000,
+  GRID_WIDTH: 10,
+  GRID_HEIGHT: 20,
+} as const;
+
+const Block = {
+  WIDTH: Viewport.CANVAS_WIDTH / Constants.GRID_WIDTH,
+  HEIGHT: Viewport.CANVAS_HEIGHT / Constants.GRID_HEIGHT,
+};
+
+/** User input */
+
+type Key = "KeyS" | "KeyA" | "KeyD";
+
+type Event = "keydown" | "keyup" | "keypress";
+
+/** Possible input directions */
+type Direction = "left" | "right" | "down";
+
+/** Utility functions */
+
+/** TETROMINO */
+type TetrominoProps = Readonly<{
+  height: number,
+  width: number,
+  x: number,
+  y: number,
+  style: string,
+}>;
+
+type Tetromino = Readonly<{
+  shape: number[][],
+  topLeft: { row: number, col: number }
+  potentialX: number,
+  potentialY: number,
+  props: TetrominoProps
+}>;
+
+/** State processing */
+
+type State = Readonly<{
+  grid: number[][];
+  gameEnd: boolean;
+  col: number;
+  row: number;
+  currentTetromino: Tetromino;
+}>;
